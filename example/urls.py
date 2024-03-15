176c60ba48dede2ff2de70d5a38a6e47642963db
fmtawhid <tawhid0406@gmail.com>
2024-03-16 03:17:28 +0600

    chage sum name

--- example/urls.py
+++ example/urls.py
@@ -5,7 +5,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='home'),
     path('4th/', fourth, name='4th'),
     path('5th/', fifth, name='5th'),
     path('6th/', six, name='6th'),
